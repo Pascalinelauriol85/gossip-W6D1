1db2f1ea90d825c6e8eacad2da509594059bdad0
pascalinelauriol85 <pascalinelauriol85@gmail.com>
2021-05-10 19:29:15 +0200

    version 2

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,4 +4,17 @@ class StaticController < ApplicationController
 
   def contact
   end
+
+  def welcome
+  end
+
+  def home
+    @gossips = Gossip.all 
+   end
+
+   def show
+    @gossips = Gossip.find{params[:id]}
+   end
+ 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  
+  root 'static#home'
+
   get '/team', to: 'static#team'
+
   get '/contact', to: 'static#contact'
+
+  get '/welcome/:first_name', to: 'static#welcome'
+
+  get '/gossips/:id', to: 'static#show', as:'gossip'
+  
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
